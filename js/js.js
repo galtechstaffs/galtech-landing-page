c1b9094b57be49d6a17cc2d039f3752fca53251e
sivaprasadsiva <sivaprasadsivank@gmail.com>
2024-03-20 11:53:33 +0530

    commit20

--- js/js.js
+++ js/js.js
@@ -1,11 +1,11 @@
-window.addEventListener('scroll', function () {
-    var scroll = window.pageYOffset;
+window.addEventListener("scroll", function () {
+  var scroll = window.pageYOffset;
 
-    if (scroll >= 100) {
-        document.body.classList.add('scroll-start');
-    } else {
-        document.body.classList.remove('scroll-start');
-    }
+  if (scroll >= 100) {
+    document.body.classList.add("scroll-start");
+  } else {
+    document.body.classList.remove("scroll-start");
+  }
 });
 
 // var navLinks = document.querySelectorAll('.nav-items');
@@ -22,88 +22,89 @@ window.addEventListener('scroll', function () {
 //     });
 // });
 
-document.querySelector(".each-accordionbox:first-child").classList.add("active");
-document.querySelector(".each-accordionbox:first-child .each-text").style.display = "flex";
+document
+  .querySelector(".each-accordionbox:first-child")
+  .classList.add("active");
+document.querySelector(
+  ".each-accordionbox:first-child .each-text"
+).style.display = "flex";
 document.querySelectorAll(".each-title").forEach(function (title) {
-    title.addEventListener("click", function () {
-        var accordionBox = this.parentNode;
-        var accordionText = this.nextElementSibling;
+  title.addEventListener("click", function () {
+    var accordionBox = this.parentNode;
+    var accordionText = this.nextElementSibling;
 
-        if (accordionBox.classList.contains("active")) {
-            accordionText.style.display = "none";
-            accordionBox.classList.remove("active");
-        } else {
-            document.querySelectorAll(".each-text").forEach(function (text) {
-                text.style.display = "none";
-            });
-            document.querySelectorAll(".each-accordionbox").forEach(function (box) {
-                box.classList.remove("active");
-            });
-            accordionBox.classList.add("active");
-            accordionText.style.display = "flex";
-        }
-    });
+    if (accordionBox.classList.contains("active")) {
+      accordionText.style.display = "none";
+      accordionBox.classList.remove("active");
+    } else {
+      document.querySelectorAll(".each-text").forEach(function (text) {
+        text.style.display = "none";
+      });
+      document.querySelectorAll(".each-accordionbox").forEach(function (box) {
+        box.classList.remove("active");
+      });
+      accordionBox.classList.add("active");
+      accordionText.style.display = "flex";
+    }
+  });
 });
 
 // Select all elements with the class "btn-modal" and add a click event listener to each one
-document.querySelectorAll(".btn-modal").forEach(function(btn) {
-    btn.addEventListener('click', function () {
-        var body = document.body;
+document.querySelectorAll(".btn-modal").forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    var body = document.body;
 
-        // Toggle the class "open" on the body element
-        body.classList.toggle('open');
-    });
+    // Toggle the class "open" on the body element
+    body.classList.toggle("open");
+  });
 });
 
-
-document.querySelector(".close-btn").addEventListener('click', function (event) {
+document
+  .querySelector(".close-btn")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    
-    document.body.classList.remove('open');
-});
 
+    document.body.classList.remove("open");
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-    if (window.innerWidth < 990) {
-        var textContainers = document.querySelectorAll('.textContainer');
-        var viewMoreBtns = document.querySelectorAll('.viewMoreBtn');
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.innerWidth < 990) {
+    var textContainers = document.querySelectorAll(".textContainer");
+    var viewMoreBtns = document.querySelectorAll(".viewMoreBtn");
 
-        textContainers.forEach(function (textContainer, index) {
-            textContainer.style.maxHeight = '125px'; // Initialize the max height
+    textContainers.forEach(function (textContainer, index) {
+      textContainer.style.maxHeight = "125px"; // Initialize the max height
 
-            viewMoreBtns[index].addEventListener('click', function () {
-                if (textContainer.style.maxHeight) {
-                    textContainer.style.maxHeight = null;
-                    viewMoreBtns[index].textContent = 'Read Less';
-                } else {
-                    textContainer.style.maxHeight = '125px';
-                    viewMoreBtns[index].textContent = 'Read More';
-                }
-            });
-        });
-    }
-
-
-
-    document.querySelector(".switch").addEventListener('click', function() {
-        toggleMenuFunction();
-    });
-
-
-
-    function toggleMenuFunction() {
-        var selector = document.querySelector(".body");
-
-        if (selector.classList.contains('light')) {
-            selector.classList.remove('light');
+      viewMoreBtns[index].addEventListener("click", function () {
+        if (textContainer.style.maxHeight) {
+          textContainer.style.maxHeight = null;
+          viewMoreBtns[index].textContent = "Read Less";
         } else {
-            selector.classList.add('light');
+          textContainer.style.maxHeight = "125px";
+          viewMoreBtns[index].textContent = "Read More";
         }
+      });
+    });
+  }
+
+  document.querySelector(".switch").addEventListener("click", function () {
+    toggleMenuFunction();
+  });
+
+
+  function toggleMenuFunction() {
+    var body = document.querySelector(".body");
+    body.classList.toggle("light");
+
+    var isDarkModeEnabled = body.classList.contains("light");
+    localStorage.setItem("darkModeEnabled", isDarkModeEnabled);
+  }
+  function initializeDarkMode() {
+    var isDarkModeEnabled = JSON.parse(localStorage.getItem("darkModeEnabled"));
+    if (isDarkModeEnabled) {
+      document.querySelector(".body").classList.add("light");
     }
+  }
 
-
-    
-
+  window.onload = initializeDarkMode;
 });
-
-
